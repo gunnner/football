4fe51d296234ae5a7f5d3c0729bfdd9431fcf951
gunnner <ctrlgrand@gmail.com>
2026-04-17 15:56:36 +0300

    Update Rails and related gems to version 8.1.3 in Gemfile and Gemfile.lock
    - Upgraded Rails and associated gems to version 8.1.3 for improved performance and security.
    - Updated addressable gem to version 2.9.0 and regexp_parser to version 2.12.0.
    - Adjusted CI configuration by removing unnecessary step for Ruby style checking.

--- config/ci.rb
+++ config/ci.rb
@@ -4,7 +4,6 @@ CI.run do
   step 'Setup', 'bin/setup --skip-server'
 
   step 'Style: Ruby', 'bin/rubocop'
-
   step 'Security: Gem audit', 'bin/bundler-audit'
   step 'Security: Importmap vulnerability audit', 'bin/importmap audit'
   step 'Security: Brakeman code analysis', 'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
